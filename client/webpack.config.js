3bd20d9cb77d989773f9aa098e899236dfca5ae4
manavj01 <manavj0120@gmail.com>
2021-09-30 21:28:12 +0530

    client: UI Initial Setup

---
+++ client/webpack.config.js
@@ -0,0 +1,49 @@
+const prod = process.env.NODE_ENV === 'production';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: prod ? 'production' : 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: __dirname + '/dist/',
+  },
+  resolve: {
+    extensions: ['*', ".js", ".jsx", '.ts', '.tsx'],
+    alias: {
+      "@components": path.resolve(__dirname, "src/components/"),
+      "@lib": path.resolve(__dirname, "src/lib/"),
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.ts', '.tsx', '.js', '.json'],
+        },
+        use: 'ts-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ]
+  },
+  devtool: prod ? undefined : 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
